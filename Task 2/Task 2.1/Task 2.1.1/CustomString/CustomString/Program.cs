b47480adf0f444d72756fe05ea050063b55ff1d1
danil <daniil.anef@gmail.com>
2022-01-23 12:32:16 +0400

    added Task 2.1.1 with Custom String Library (dll)

---
+++ Task 2/Task 2.1/Task 2.1.1/CustomString/CustomString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UpdatedStringClass;
+namespace CustomString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            myString str = new myString("Hello World!");
+            Console.WriteLine(str.ToString());
+            myString str2 = new myString("Hello Everyone!");
+            str += str2;
+            Console.WriteLine(str.ToString());
+            Console.WriteLine(str > str2);
+            Console.WriteLine(str < str2);
+            Console.WriteLine((str * str2).ToString());
+            myString notes = myString.makeNotesString();
+            for(int i = 0; i < notes.lenght; i++)
+            {
+                Console.WriteLine(notes[i]);
+            }
+        }
+    }
+}
